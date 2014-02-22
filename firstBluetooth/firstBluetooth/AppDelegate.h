c610a2bda05611c8384b339cec2ee4d6da85ae0a
e115715 <e115715@ie.u-ryukyu.ac.jp>
2014-02-22 15:11:05 +0900

    First commit

---
+++ firstBluetooth/firstBluetooth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  firstBluetooth
+//
+//  Created by Matsuda Yuta on 2014/02/22.
+//  Copyright (c) 2014年 e115715. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ firstBluetooth/firstBluetooth/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  firstBluetooth
+//
+//  Created by Matsuda Yuta on 2014/02/22.
+//  Copyright (c) 2014年 e115715. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
